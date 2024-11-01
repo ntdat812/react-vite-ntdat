b953d1c5323c17c51c38aa3dd6d48185847d540e
ntdat <nguyenthanhdatbi812@gmail.com>
2024-11-01 21:15:41 +0700

    video 36: DOM event

--- src/components/todo/ToDoNew.jsx
+++ src/components/todo/ToDoNew.jsx
@@ -1,11 +1,27 @@
 
 const TodoNew = (props) => {
     const { addNewTodo } = props;
-    addNewTodo("dat");
+    //  addNewTodo("dat");
+
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleOnChange = (event) => {
+        console.log(">>Handle on change", event.target.value)
+    }
     return (
         <div className="addContainer">
-            <input type="text" className="input-highlight" placeholder="Nhập nội dung" />
-            <button className="button-add">Add</button>
+            <input
+                type="text"
+                className="input-highlight"
+                placeholder="Nhập nội dung"
+                onChange={handleOnChange}
+            />
+            <button
+                className="button-add"
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
