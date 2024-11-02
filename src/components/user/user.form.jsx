eeea788809fe9ac53b4642bb4979c7400c3bee4e
ntdat <nguyenthanhdatbi812@gmail.com>
2024-11-02 17:34:51 +0700

    video 70

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,7 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/apiService";
+
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -8,16 +9,16 @@ const UserForm = () => {
     const [password, setPassWord] = useState("");
     const [phone, setPhoneNumber] = useState("");
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        if (res.data) {
+            notification.success({
+                message: "Create user",
+                description: "Tạo mới user thành công!"
+            })
         }
-        axios.post(URL_BACKEND, data)
-        console.log(">>check state: ", { fullName, email, password, phone });
+
+        console.log(">>res: ", res.data)
     }
 
     console.log("check fullname: ", fullName)
