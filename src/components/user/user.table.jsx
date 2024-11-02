515de1198880bd57b3a139f82de491c39aa07691
ntdat <nguyenthanhdatbi812@gmail.com>
2024-11-02 23:40:26 +0700

    video 72

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,80 +1,42 @@
 
 import { Space, Table, Tag } from 'antd';
+import { fetchAllUserAPI } from '../../services/apiService';
+import { useState } from 'react';
 
 const UserTable = () => {
+    const [dataUser, serDataUser] = useState([
+        {
+            _id: "dat", fullName: 21, email: "thanhhoa"
+        },
+        {
+            _id: "dat", fullName: 21, email: "thanhhoa"
+        }
+    ]);
     const columns = [
         {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-            render: (text) => <a>{text}</a>,
+            title: 'Id',
+            dataIndex: '_id',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Full name',
+            dataIndex: 'fullName',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
-        },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_, { tags }) => (
-                <>
-                    {tags.map((tag) => {
-                        let color = tag.length > 5 ? 'geekblue' : 'green';
-                        if (tag === 'loser') {
-                            color = 'volcano';
-                        }
-                        return (
-                            <Tag color={color} key={tag}>
-                                {tag.toUpperCase()}
-                            </Tag>
-                        );
-                    })}
-                </>
-            ),
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <a>Invite {record.name}</a>
-                    <a>Delete</a>
-                </Space>
-            ),
-        },
+            title: 'Email',
+            dataIndex: 'email',
+        }
     ];
-    const data = [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            tags: ['nice', 'developer'],
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            tags: ['loser'],
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sydney No. 1 Lake Park',
-            tags: ['cool', 'teacher'],
-        },
-    ];
+
+    const loadUser = async () => {
+        console.log(">>> run render")
+        const res = await fetchAllUserAPI()
+        //serDataUser(res.data)
+    }
+
+    loadUser()
+
     return (
-        <Table columns={columns} dataSource={data} />
+        <Table columns={columns} dataSource={dataUser} />
     )
 }
 
