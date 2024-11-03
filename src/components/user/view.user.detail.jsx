bf2e25c084785ac1c4b2ba22c10787ad8130a9cf
ntdat <nguyenthanhdatbi812@gmail.com>
2024-11-03 16:20:21 +0700

    video 82

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -5,7 +5,9 @@ const ViewUserDetail = (props) => {
     const { setDataDetail, dataDetail, setIsDetailOpen, isDetailOpen } = props
     console.log("Check data detail: ", dataDetail)
     return (
-        <Drawer title="Chi tiết User"
+        <Drawer
+            width={"40vw"}
+            title="Chi tiết User"
             onClose={() => {
                 setIsDetailOpen(false)
                 setDataDetail("")
@@ -19,6 +21,26 @@ const ViewUserDetail = (props) => {
                 <p>Email:  {dataDetail.email}</p>
                 <br />
                 <p>Phone name: {dataDetail.phone}</p>
+                <br />
+                <p>Avatar:</p>
+                <div>
+                    <img
+                        height={120} width={120} src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                </div>
+                <div>
+                    <label htmlFor="btnUpload" style={{
+                        display: "block",
+                        width: "fit-content",
+                        marginTop: "15px",
+                        padding: "5px 10px",
+                        background: "orange ",
+                        borderRadius: " 5px",
+                        cursor: "pointer",
+                    }}  >Upload avatar</label>
+                    <input type="file" hidden id="btnUpload" />
+                </div>
+                {/* <Button type="primary" >Upload file</Button> */}
+
             </>
                 :
                 <>
@@ -27,7 +49,7 @@ const ViewUserDetail = (props) => {
                 </>
             }
 
-        </Drawer>
+        </Drawer >
     )
 }
 
